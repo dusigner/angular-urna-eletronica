3207b59a699c5594051a25b7c23a4e5cf4d06890
ln00753 <eduardo@telefonicaed.com.br>
2018-12-05 15:22:23 -0200

    create numbers and display

--- src/app/urna/components/urna.component.ts
+++ src/app/urna/components/urna.component.ts
@@ -9,9 +9,46 @@ import { UrnaService } from '../services';
 })
 export class UrnaComponent implements OnInit {
 
+  private numero1: string;
+  private numero2: string;
+
   constructor(private urnaService: UrnaService) { }
 
   ngOnInit() {
+    this.corrigir();
   }
 
+  /**
+   * Botão Corrigir
+   * @return void
+   */
+
+  corrigir(): void {
+    this.numero1 = null;
+    this.numero2 = null;
+  }
+
+  adicionarNumero(numero: string): void {
+    if (this.numero1 === null) {
+      this.numero1 = numero;
+    } else if (this.numero2 === null) {
+        this.numero2 = numero;
+    } else {
+      console.log('Já votou');
+    }
+  }
+
+  get displayNumero1(): string {
+    if (this.numero1 !== null) {
+      return this.numero1;
+    }
+    return this.numero1;
+  }
+
+  get displayNumero2(): string {
+    if (this.numero2 !== null) {
+      return this.numero2;
+    }
+    return this.numero2;
+  }
 }
--- src/app/urna/services/urna.service.ts
+++ src/app/urna/services/urna.service.ts
@@ -1,3 +1,9 @@
+/**
+ * Serviço responsável por executar a urna eletrônica
+ * @author Eduardo Soares <eduardo@dusigner.com.br>
+ * @since 1.0.0
+ */
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,4 +12,5 @@ import { Injectable } from '@angular/core';
 export class UrnaService {
 
   constructor() { }
+
 }
